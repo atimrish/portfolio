814ab5f0308a2b75ed1554aeb553248fd925a13d
atimrish <atimrish11@gmail.com>
2025-01-10 17:55:55 +0400

    [add] компонент WorkCard

---
+++ src/widgets/work-card/ui/WorkCard.stories.tsx
@@ -0,0 +1,28 @@
+import {WorkCard} from './WorkCard'
+import {Meta, StoryObj} from "@storybook/react"
+import {Typography} from "@src/shared/ui/typography";
+
+import ReactIcon from '@src/shared/ui/assets/images/react.png'
+import ExpressIcon from '@src/shared/ui/assets/images/express.png'
+
+const meta: Meta<typeof WorkCard> = {
+    title: 'Компоненты/WorkCard',
+    component: WorkCard,
+}
+
+export default meta
+
+type Story = StoryObj<typeof WorkCard>
+
+
+
+export const Default: Story = {
+    args: {
+        year: 2024,
+        month: 'август',
+        job: 'Fullstack Developer',
+        durationTime: '1 год',
+        renderDescription: () => <Typography.Paragraph>Какое-то описание</Typography.Paragraph>,
+        techsImageSources: [ReactIcon, ExpressIcon],
+    }
+}
---
+++ src/widgets/work-card/ui/WorkCard.tsx
@@ -0,0 +1,93 @@
+import styled, {css} from "styled-components";
+import {ReactNode} from "react";
+import {Theme} from "@src/app/providers/themes";
+import {useTheme} from "@src/app/providers/ThemeProvider";
+import {Typography} from "@src/shared/ui/typography";
+
+const Container = styled.div<{ theme: Theme }>`
+    width: clamp(292px, 91.25vw, 542px);
+    padding: clamp(14px, 4.375vw, 25px);
+    border: 6px solid ${props => props.theme.borderColor};
+
+    @media screen and (max-width: 560px) {
+        border-width: 4px;
+    }
+`
+
+const HeadingBlock = styled.div`
+    display: flex;
+    gap: 10px;
+    align-items: end;
+`
+
+const TechBlock = styled.div`
+    display: flex;
+    gap: clamp(5px, 1.5625vw, 18px);
+`
+
+const TechImage = styled.img`
+    width: clamp(28px, 8.75vw, 48px);
+    aspect-ratio: 1;
+    object-fit: cover;
+    object-position: center;
+`
+
+const DescriptionBlock = styled.div`
+    margin: 25px 0;
+`
+
+const YearStyles = css`
+    font-size: clamp(20px, 6.25vw, 48px);
+    font-weight: 400;
+`
+
+const MonthStyles = css`
+    font-size: clamp(14px, 4.375vw, 32px);
+    font-weight: 400;
+`
+
+const DurationStyles = css`
+    margin-top: 6px;
+    font-size: clamp(14px, 4.375vw, 32px);
+    font-weight: 400;   
+`
+
+const JobStyles = css`
+    margin: 25px 0;
+    font-size: clamp(20px, 6.25vw, 40px);
+    font-weight: 400;
+`
+
+type Props = {
+    year: number,
+    month: string,
+    durationTime?: string,
+    job: string,
+    techsImageSources: string[],
+    renderDescription: () => ReactNode,
+}
+
+export const WorkCard = (p: Props) => {
+    const {theme} = useTheme()
+
+    return (
+        <Container theme={theme}>
+            <HeadingBlock>
+                <Typography.Text css={YearStyles}>{p.year}</Typography.Text>
+                <Typography.Text css={MonthStyles}>{p.month}</Typography.Text>
+            </HeadingBlock>
+
+            {p.durationTime && <Typography.Text css={DurationStyles}>{p.durationTime}</Typography.Text>}
+
+            <Typography.Text css={JobStyles}>{p.job}</Typography.Text>
+
+            <DescriptionBlock>
+                {p.renderDescription()}
+            </DescriptionBlock>
+
+            <TechBlock>
+                {p.techsImageSources.map(i => <TechImage src={i} alt=""/>)}
+            </TechBlock>
+        </Container>
+    );
+};
